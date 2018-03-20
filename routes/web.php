578d9cccf23b3fc669cf4e766c3c6f6784093848
Victor Alves <vsilva.programador@gmail.com>
2018-03-20 18:29:05 -0300

    First Controller Initialized

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ Route::get('/', 'PagesController@index');
 Route::get('/about', 'PagesController@about');
 
 Route::get('/services', 'PagesController@services');
+
+Route::resource('posts', 'PostsController');
