d5479f5f4fd2379f655d482db5113a44e0e24258
Victor Alves <vsilva.programador@gmail.com>
2018-03-26 20:37:09 -0300

    Added relationship between User and Post

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,6 +15,34 @@
                     @endif
                     
                     <h3>Your Blog Posts</h3>
+                    @if(isset($posts) && count($posts) > 0)
+                        <table class="table table-striped">
+                            <thead>
+                                <th>Title</th>
+                                <th></th>
+                                <th></th>
+                            </thead>
+                            <tbody>
+                                @foreach($posts as $post)                      
+                                   <tr>
+                                        <td>{{$post->title}}</td>
+                                        <td>
+                                            <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+                                        </td>
+                                        <td>
+                                            {!!Form::open(['action'=>['PostsController@destroy', $post->id], 'method' => 'POST', 'class'=>'pull-right' ])!!}        
+                                                {{Form::hidden('_method', 'DELETE')}}
+                                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                                {{Form::submit('Delete', ['class'=>'btn btn-danger'])}}
+                                            {!!Form::close()!!}
+                                        </td>
+                                    </tr> 
+                                @endforeach
+                            </tbody>
+                        </table>
+                    @else
+                        You don't have any posts.
+                    @endif
                 </div>
             </div>
         </div>
